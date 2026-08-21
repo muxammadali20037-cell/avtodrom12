96ad9828a138903011c480014e2f120284692b21
muxammadali20037-cell <muxammadali20037@gmail.com>
2026-08-21 13:09:40 +0300

    Adapt freeze API responses for Vercel serverless handler

--- api/index.js
+++ api/index.js
@@ -2,7 +2,19 @@ import app from "../backend/src/server.js";
 import { handleFreezeRequest } from "../backend/src/freeze-routes.js";
 
 export default async function handler(req, res) {
-  const handled = await handleFreezeRequest(req, res);
+  const jsonRes = {
+    status(code) {
+      res.statusCode = code;
+      return this;
+    },
+    json(data) {
+      if (!res.headersSent) res.setHeader("Content-Type", "application/json; charset=utf-8");
+      res.end(JSON.stringify(data));
+      return this;
+    }
+  };
+
+  const handled = await handleFreezeRequest(req, jsonRes);
   if (handled !== null) return handled;
   return app(req, res);
 }
