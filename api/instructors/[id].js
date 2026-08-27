a17b837b010a23cd5979e24ea7f8604173789916
muxammadali20037-cell <muxammadali20037@gmail.com>
2026-08-27 06:03:15 +0300

    Use safe V3 instructor update schema

--- api/instructors/[id].js
+++ api/instructors/[id].js
@@ -1,7 +1,7 @@
-import { handleV3Request } from '../../backend/src/v3-routes.js';
+import { handleSafeV3 } from '../../backend/src/v3-safe-routes.js';
 
 export default async function handler(req, res) {
-  const handled = await handleV3Request(req, res);
+  const handled = await handleSafeV3(req, res);
   if (!handled && !res.writableEnded) {
     res.statusCode = 404;
     res.setHeader('Content-Type', 'application/json; charset=utf-8');
